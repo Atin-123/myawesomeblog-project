15016516a3e1d616c5fad6957f3f3b15e47b02e9
ATINDRA PAL <atindra8016@gmail.com>
2021-10-26 23:33:14 +0530

    Implement All features

--- myawesomeblog/urls.py
+++ myawesomeblog/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls.conf import include
+import events.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', events.views.home, name="home"),
+    path('posts/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
